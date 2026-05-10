b599d8c221e284aa6b6bcc4d0b3d4dca52179a19
Petr Tsopa <petrtsopa03@gmail.com>
2026-05-10 20:51:27 +0300

    disable flight

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,3 +1,15 @@
-// Visit the wiki for more info - https://kubejs.com/
-console.info('Hello, World! (Loaded server example script)')
+ServerEvents.recipes((event) => {
+    const noCraft = [
+        'ars_nouveau:stable_warp_scroll',
+        'ars_nouveau:relay_warp',
+        'ars_nouveau:ritual_warping',
+        'advanced_ae:flight_card',
+        'advanced_ae:flight_drift_card'
+    ]
 
+    for (const id of noCraft) {
+        event.remove({ output: id })
+    }
+
+    event.remove({ id: 'ars_nouveau:tomes/warp_impact_tome' })
+})
